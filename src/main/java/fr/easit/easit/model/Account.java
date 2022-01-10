af47f4fe395801284546fad0626725ea1aa07467
Cassius-P <Mfu08vjv>
2022-01-10 11:49:41 +0100

    Model Account.java

---
+++ src/main/java/fr/easit/easit/model/Account.java
@@ -0,0 +1,71 @@
+package fr.easit.easit.model;
+
+
+import java.util.Objects;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "accounts")
+
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    public Account(){}
+
+    public Account(String username, String password, String email){
+        setUsername(username);
+        setPassword(password);
+        setEmail(email);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("City{");
+        sb.append("id=").append(id);
+        sb.append(", username='").append(username).append('\'');
+        sb.append(", password=").append(password).append('\'');
+        sb.append(", email=").append(email);
+        sb.append('}');
+        return sb.toString();
+    }
+}
