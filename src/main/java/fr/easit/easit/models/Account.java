2db6be2c1fb13209b5f499084be6684bbf550507
Cassius-P <Mfu08vjv>
2022-01-10 16:53:36 +0100

    Log + fix toString method for Account Model

--- src/main/java/fr/easit/easit/models/Account.java
+++ src/main/java/fr/easit/easit/models/Account.java
@@ -59,7 +59,7 @@ public class Account {
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("City{");
+        final StringBuilder sb = new StringBuilder("Account{");
         sb.append("id=").append(id);
         sb.append(", username='").append(username).append('\'');
         sb.append(", password=").append(password).append('\'');
--- src/main/java/fr/easit/easit/services/AccountService.java
+++ src/main/java/fr/easit/easit/services/AccountService.java
@@ -2,14 +2,17 @@ package fr.easit.easit.services;
 
 import fr.easit.easit.models.Account;
 import fr.easit.easit.repositories.IAccountRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import org.slf4j.Logger;
 
 @Service
 public class AccountService implements IAccountService {
+
+    Logger log = LoggerFactory.getLogger(AccountService.class);
 
     @Autowired
     private IAccountRepository repository;
@@ -18,6 +21,9 @@ public class AccountService implements IAccountService {
     @Override
     public List<Account> findAll() {
         List<Account> accounts = repository.findAll();
+        for (Account a : accounts ) {
+            log.info(a.toString());
+        }
         return accounts;
     }
 }
