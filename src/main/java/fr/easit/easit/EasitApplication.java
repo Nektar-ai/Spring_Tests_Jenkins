aeac3a58336c16b7db1867dacc9f87ef91a5c27e
Cassius-P <Mfu08vjv>
2022-01-10 09:25:54 +0100

    Initial commit

---
+++ src/main/java/fr/easit/easit/EasitApplication.java
@@ -0,0 +1,13 @@
+package fr.easit.easit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/easit/easit/EasitApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.easit.easit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
