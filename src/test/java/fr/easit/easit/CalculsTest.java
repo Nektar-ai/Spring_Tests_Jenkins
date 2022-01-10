7f3659d6eb9f32bff9d3d3a328d0077404deed7e
Nektar-ai <nektar.ai@gmail.com>
2022-01-10 12:07:21 +0100

    pom.xml + test class

---
+++ src/test/java/fr/easit/easit/CalculsTest.java
@@ -0,0 +1,34 @@
+package fr.easit.easit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void multiplier() {
+    }
+
+    @Test
+    void additionner() {
+    }
+
+    @Test
+    void diviser() {
+    }
+
+    @Test
+    void soustraire() {
+    }
+}
