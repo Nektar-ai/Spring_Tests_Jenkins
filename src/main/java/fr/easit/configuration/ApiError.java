52455db5579874303817fd28bc6a29c51e14769a
Ilias <ilias.kelesoglu@globule.net>
2022-03-04 00:21:35 +0100

    Added statusCode in apiError

--- src/main/java/fr/easit/configuration/ApiError.java
+++ src/main/java/fr/easit/configuration/ApiError.java
@@ -10,9 +10,15 @@ import java.util.Arrays;
 import java.util.List;
 
 @Data
-@RequiredArgsConstructor
 public class ApiError {
 
+    public ApiError(HttpStatus httpStatus, String message){
+        this.httpStatus = httpStatus;
+        this.message = message;
+        this.statusCode = httpStatus.value();
+    }
+
+    private final Integer statusCode;
     private final HttpStatus httpStatus;
     private final String message;
 
@@ -22,5 +28,9 @@ public class ApiError {
 
     public String getMessage() {
         return message;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
     }
 }
